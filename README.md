5233ed941de23760a7d0684e5ca493825eafe3e3
the-human-guy <170432371+the-human-guy@users.noreply.github.com>
2025-06-04 18:23:05 +0300

    htmlUntouched, unescaped characters

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
+## run
+
 `python ./serve_notes.py`
 
 OR 
 
 `python ./serve_notes.py --dir ~/path_to_notes --port 8000`
 
+OR V2
+
+`python ./serve_notes_v2.py --dir ~/tmp --fe-script-name frontend-froala.js`
+
+## done
+- stop messing up proper ul>li lists (or use plaintext lists instead) -- fixed with `htmlUntouched: true`
+- don't escape anything except mandatory (&, >, <) -- fixed with `entities: '&amp;&lt;&gt;'`
+
+
+## todo
+- don't turn `<b>` into `<strong>`
--- frontend-froala.js
+++ frontend-froala.js
@@ -1,121 +1,123 @@
 const $log = console.log.bind(console);
 const body = document.body;
 
-
-
 function initEditor() {
-    const imgsToBeRemovedFromServer = []
-    
-    // Init Summernote
-    $(document).ready(function () {
-        window.editor = new FroalaEditor('#editor-anchor', {
-            multiline: true,
-            /* htmlUntouched: true, */
-             codeBeautifierOptions: {
-               end_with_newline: true,
-           indent_inner_html: true,
-               extra_liners: "['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'pre', 'ul', 'ol', 'table', 'dl']",
-               brace_style: 'expand',
-               indent_char: ' ',
-               indent_size: 4,
-               wrap_line_length: 0
-             },
-            enter: FroalaEditor.ENTER_BR,
-            saveURL: window.location.pathname,
-            saveMethod: 'PUT',
-            events: {
-                'save.before': function () {
-                  // Before save request is made.
-                },
-          
-                'save.after': function () {
-                    imgsToBeRemovedFromServer.forEach(imgUrl => {
-                        fetch(imgUrl, { method: 'DELETE' })
-                    })
-                    imgsToBeRemovedFromServer.length = 0
+  const imgsToBeRemovedFromServer = [];
 
-                },
-          
-                'save.error': function () {
-                  // Do something here.
-                },
+  // Init Summernote
+  $(document).ready(function () {
+    window.editor = new FroalaEditor("#editor-anchor", {
+      multiline: true,
+      htmlUntouched: true, // prevents froala from collapsing ul>li lists
+      entities: '&amp;&lt;&gt;', // Only escape &, <, >
+      // codeBeautifierOptions: {
+      //   htmlAllowedTags: 
+      // },
+      // codeBeautifierOptions: {
+      //   end_with_newline: true,
+      //   indent_inner_html: true,
+      //   htmlAllowedTags: 
+      //   extra_liners: "['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'pre', 'ul', 'li',s 'ol', 'table', 'dl']",
+      //   brace_style: "expand",
+      //   indent_char: " ",
+      //   indent_size: 4,
+      //   wrap_line_length: 0,
+      // },
+      enter: FroalaEditor.ENTER_BR,
+      saveURL: window.location.pathname,
+      saveMethod: "PUT",
+      events: {
+        "save.before": function () {
+          // Before save request is made.
+        },
 
-                'image.removed': function ($img) {
-                    imgsToBeRemovedFromServer.push(new URL($img[0].src).pathname)
-                  }
-              },
-              saveInterval: 10000, // default
-              imageUploadURL: '/upload-image',
-              imageUploadParams: {
-                filePath: location.pathname,
-              },
-           })
+        "save.after": function () {
+          imgsToBeRemovedFromServer.forEach((imgUrl) => {
+            fetch(imgUrl, { method: "DELETE" });
+          });
+          imgsToBeRemovedFromServer.length = 0;
+        },
 
-        function downloadFile(file) {
-            const tempEl = document.createElement('a');
-            body.appendChild(tempEl);
-            const url = window.URL.createObjectURL(file);
-            tempEl.href = url;
-            tempEl.download = file.name;
-            tempEl.click();
-            window.URL.revokeObjectURL(url);
-        }
+        "save.error": function () {
+          // Do something here.
+        },
 
-        function downloadText(text, fileName = 'new-file.html') {
-            return downloadFile(
-                new File([new Blob([text])], fileName, { type: 'text/html' }),
-            );
-        }
-
-        window.onFileSave = function onFileSave() {
-            const text = $('#summernote').summernote('code');
-            downloadText(`<body>\n${text}\n</body>`, document.title);
-        };
-
-        window.onServerSave = async function onServerSave() {
-            window.editor.save.save()
-            // const text = $('#summernote').summernote('code');
-            // const content = `<body>\n${text}\n</body>`;
-            // const filePath = window.location.pathname;
-            // try {
-            //     const response = await fetch(filePath, {
-            //         method: 'PUT',
-            //         headers: { 'Content-Type': 'text/html' },
-            //         body: content
-            //     });
-            //     if (response.ok) {
-            //         alert('File saved successfully on server');
-            //     } else {
-            //         alert('Error saving file: ' + response.statusText);
-            //     }
-            // } catch (e) {
-            //     alert('Error saving file: ' + e);
-            // }
-        };
-
-        window.onCreateNewNote = async function onCreateNewNote() {
-            const fileName = prompt('Enter file name (e.g., new-note.html):');
-            if (!fileName) return;
-            const filePath = `/${fileName.endsWith('.html') ? fileName : fileName + '.html'}`;
-            const content = `<body><pre>New Note</pre></body>`;
-            try {
-                const response = await fetch(filePath, {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'text/html' },
-                    body: content
-                });
-                if (response.ok) {
-                    alert('New note created successfully');
-                    // Redirect to the new note
-                    window.location.href = filePath;
-                } else {
-                    alert('Error creating note: ' + response.statusText);
-                }
-            } catch (e) {
-                alert('Error creating note: ' + e);
-            }
-        };
+        "image.removed": function ($img) {
+          imgsToBeRemovedFromServer.push(new URL($img[0].src).pathname);
+        },
+      },
+      // autosave
+      //   saveInterval: 10000, // default
+      saveInterval: 0, // turn autosave off
+      imageUploadURL: "/upload-image",
+      imageUploadParams: {
+        filePath: location.pathname,
+      },
     });
+
+    function downloadFile(file) {
+      const tempEl = document.createElement("a");
+      body.appendChild(tempEl);
+      const url = window.URL.createObjectURL(file);
+      tempEl.href = url;
+      tempEl.download = file.name;
+      tempEl.click();
+      window.URL.revokeObjectURL(url);
+    }
+
+    function downloadText(text, fileName = "new-file.html") {
+      return downloadFile(new File([new Blob([text])], fileName, { type: "text/html" }));
+    }
+
+    window.onFileSave = function onFileSave() {
+      const text = $("#summernote").summernote("code");
+      downloadText(`<body>\n${text}\n</body>`, document.title);
+    };
+
+    window.onServerSave = async function onServerSave() {
+      window.editor.save.save();
+      // const text = $('#summernote').summernote('code');
+      // const content = `<body>\n${text}\n</body>`;
+      // const filePath = window.location.pathname;
+      // try {
+      //     const response = await fetch(filePath, {
+      //         method: 'PUT',
+      //         headers: { 'Content-Type': 'text/html' },
+      //         body: content
+      //     });
+      //     if (response.ok) {
+      //         alert('File saved successfully on server');
+      //     } else {
+      //         alert('Error saving file: ' + response.statusText);
+      //     }
+      // } catch (e) {
+      //     alert('Error saving file: ' + e);
+      // }
+    };
+
+    window.onCreateNewNote = async function onCreateNewNote() {
+      const fileName = prompt("Enter file name (e.g., new-note.html):");
+      if (!fileName) return;
+      const filePath = `/${fileName.endsWith(".html") ? fileName : fileName + ".html"}`;
+      const content = `<body><pre>New Note</pre></body>`;
+      try {
+        const response = await fetch(filePath, {
+          method: "POST",
+          headers: { "Content-Type": "text/html" },
+          body: content,
+        });
+        if (response.ok) {
+          alert("New note created successfully");
+          // Redirect to the new note
+          window.location.href = filePath;
+        } else {
+          alert("Error creating note: " + response.statusText);
+        }
+      } catch (e) {
+        alert("Error creating note: " + e);
+      }
+    };
+  });
 }
 
 setTimeout(initEditor, 1000);
