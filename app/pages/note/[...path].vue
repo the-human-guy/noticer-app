01029da075dd7f07eeef5708341f31daa8c4b4c6
the-human-guy <170432371+the-human-guy@users.noreply.github.com>
2025-06-07 00:57:09 +0300

    note page shorter title + download btn as icon

--- app/pages/note/[...path].vue
+++ app/pages/note/[...path].vue
@@ -5,15 +5,16 @@
         <ion-buttons slot="start">
           <ion-back-button default-href="/"></ion-back-button>
         </ion-buttons>
-        <ion-title>{{ notePath }}</ion-title>
+        <ion-title>{{ noteName }}</ion-title>
         <ion-buttons slot="end">
-          <ion-button @click="onSaveNote">Save</ion-button>
           <ion-button
             @click="downloadNote"
             v-if="!isMobile"
+            aria-label="Download"
           >
-            Download
+            <ion-icon name="download-outline"></ion-icon>
           </ion-button>
+          <ion-button @click="onSaveNote">Save</ion-button>
         </ion-buttons>
       </ion-toolbar>
     </ion-header>
@@ -27,14 +28,26 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { Capacitor } from '@capacitor/core'
 import { useFileOperations } from '~/composables/useFileOperations'
+import { downloadOutline } from 'ionicons/icons'
+import { addIcons } from 'ionicons' // Import addIcons
+
+// Register the icon
+addIcons({
+  'download-outline': downloadOutline,
+})
 
 const froalaScript = useNuxtApp().$scripts.froala
 const route = useRoute()
 const notePath = ref<string>((route.params.path as string[]).join('/'))
+// Compute noteName by extracting the file name without path or extension
+const noteName = computed(() => {
+  const fileName = notePath.value.split('/').pop() || 'Untitled'
+  return fileName.replace(/\.[^/.]+$/, '') // Remove file extension
+})
 const noteContent = ref<string>('')
 const isMobile = Capacitor.getPlatform() !== 'web'
 const { readNote, saveNote, uploadImage, deleteImage } = useFileOperations()
@@ -113,7 +126,7 @@ function downloadNote() {
   const url = URL.createObjectURL(blob)
   const a = document.createElement('a')
   a.href = url
-  a.download = notePath.value.split('/').pop() || 'note.html'
+  a.download = noteName.value + '.html' // Use noteName for download file name
   a.click()
   URL.revokeObjectURL(url)
 }
