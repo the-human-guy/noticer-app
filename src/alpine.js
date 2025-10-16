a754938593994680251868ad60689ea495d11ffe
the-human-guy <170432371+the-human-guy@users.noreply.github.com>
2025-10-16 19:05:05 +0700

    add tinyMCE and init

--- src/alpine.js
+++ src/alpine.js
@@ -3,7 +3,7 @@ const { dialog, fs } = window.__TAURI__;
 
 const alp = Alpine
 
-const $Files = () => Alpine.store('Files')
+const $Files = () => alp.store('Files')
 alp.store('Files', {
   items: [],
   init() {
@@ -12,8 +12,8 @@ alp.store('Files', {
       this.readSelectedDir()
     }
   },
-  userSelectedPaths: Alpine.$persist({}).as('userSelectedPaths'),
-  selectedDirPath: Alpine.$persist('').as('selectedDirPath'),
+  userSelectedPaths: alp.$persist({}).as('userSelectedPaths'),
+  selectedDirPath: alp.$persist('').as('selectedDirPath'),
   async pickDir() {
     // picked by user through the dialog.
     // these paths have read permissions.
@@ -84,3 +84,85 @@ alp.store('Files', {
     await file.close();
   }
 })
+
+
+alp.store('Editor', {
+  items: [],
+  init(id) {
+    tinymce.init({
+      selector: '#' + id,
+      license_key: 'gpl',
+      plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons accordion',
+      menubar: 'file edit view insert format tools table help',
+      toolbar: "undo redo | accordion accordionremove | blocks fontfamily fontsize | bold italic underline strikethrough | align numlist bullist | link image | table media | lineheight outdent indent| forecolor backcolor removeformat | charmap emoticons | code fullscreen preview | save print | pagebreak anchor codesample | ltr rtl",
+      contextmenu: 'link image table',
+      skin: 'oxide',
+      quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+    }).then(() => {
+      // add 'input' listener 
+      tinymce.get(id).on('input', () => {
+        // set textarea value = editor value
+        document.getElementById(id).value = tinymce.get(id).getContent();
+        // dispatch a native event for Alpine to recognize
+        document.getElementById(id).dispatchEvent(new Event('input', { bubbles: true }));
+      })
+    }); 
+  },
+})
+
+
+
+// Full featured demo: Non-Premium Plugins only
+/*
+const useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const isSmallScreen = window.matchMedia('(max-width: 1023.5px)').matches;
+
+tinymce.init({
+  selector: 'textarea#open-source-plugins',
+  plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons accordion',
+  editimage_cors_hosts: ['picsum.photos'],
+  menubar: 'file edit view insert format tools table help',
+  toolbar: "undo redo | accordion accordionremove | blocks fontfamily fontsize | bold italic underline strikethrough | align numlist bullist | link image | table media | lineheight outdent indent| forecolor backcolor removeformat | charmap emoticons | code fullscreen preview | save print | pagebreak anchor codesample | ltr rtl",
+  autosave_ask_before_unload: true,
+  autosave_interval: '30s',
+  autosave_prefix: '{path}{query}-{id}-',
+  autosave_restore_when_empty: false,
+  autosave_retention: '2m',
+  image_advtab: true,
+  link_list: [
+    { title: 'My page 1', value: 'https://www.tiny.cloud' },
+    { title: 'My page 2', value: 'http://www.moxiecode.com' }
+  ],
+  image_list: [
+    { title: 'My page 1', value: 'https://www.tiny.cloud' },
+    { title: 'My page 2', value: 'http://www.moxiecode.com' }
+  ],
+  image_class_list: [
+    { title: 'None', value: '' },
+    { title: 'Some class', value: 'class-name' }
+  ],
+  importcss_append: true,
+  file_picker_callback: (callback, value, meta) => {
+    if (meta.filetype === 'file') {
+      callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
+    }
+
+    if (meta.filetype === 'image') {
+      callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
+    }
+
+    if (meta.filetype === 'media') {
+      callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+    }
+  },
+  height: 600,
+  image_caption: true,
+  quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+  noneditable_class: 'mceNonEditable',
+  toolbar_mode: 'sliding',
+  contextmenu: 'link image table',
+  skin: useDarkMode ? 'oxide-dark' : 'oxide',
+  content_css: useDarkMode ? 'dark' : 'default',
+  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
+});
+*/
