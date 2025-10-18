7e694bef373fe5c174e8957c4d654dffcff86b29
the-human-guy <170432371+the-human-guy@users.noreply.github.com>
2025-10-19 01:13:26 +0700

    save file -- success indicator

--- src/js/file.js
+++ src/js/file.js
@@ -1,12 +1,15 @@
 const $File = () => alp.store('File')
 alp.store('File', {
+  
+  openedFilePath: alp.$persist('').as('openedFilePath'),
+  
   init() {
     log(this.openedFilePath)
     if (this.openedFilePath) {
       this.readSelectedFile()
     }
   },
-  openedFilePath: alp.$persist('').as('openedFilePath'),
+  
   _fileContent: '',
   get fileContent() {
     return this._fileContent
@@ -14,6 +17,15 @@ alp.store('File', {
   set fileContent(newVal) {
     this._fileContent = newVal || ' '
   },
+
+  saveSuccess: false,
+  saveSuccessIndicatorTimeout: null,
+  indicateSuccessfulSave() {
+    this.saveSuccess = true
+    window.clearTimeout(this.saveSuccessIndicatorTimeout)
+    this.saveSuccessIndicatorTimeout = setTimeout(() => this.saveSuccess = false, 3000)
+  },
+
   async readSelectedFile() {
     const fileContent = await fs.readTextFile($File().openedFilePath);
     log(fileContent)
@@ -22,12 +34,16 @@ alp.store('File', {
       $Editor().initEditor()
     }, 0)
   },
+  
   async changeFile(newPath) {
     $File().openedFilePath = newPath
     $File().readSelectedFile()
   },
+
   async save() {
     await fs.writeTextFile($File().openedFilePath, $Editor().save())
-  }
+    $File().indicateSuccessfulSave()
+  },
+
 })
 
